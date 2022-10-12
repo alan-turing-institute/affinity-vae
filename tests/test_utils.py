997d886af9d758be7303b3dd0928d0cbc6856ee2
quantumjot <code@arlowe.co.uk>
2022-10-12 10:41:46 +0100

    add simple tests

---
+++ tests/test_utils.py
@@ -0,0 +1,13 @@
+import pytest
+
+from affinivae.utils import dims_after_pooling
+
+
+@pytest.mark.parametrize(
+    "n_pools, expected", [(0, 64), (1, 32), (2, 16), (3, 8)]
+)
+def test_dims_after_pooling_ndim(n_pools, expected):
+    """Test dimension calculation after 2x2 pooling op."""
+    start = 64
+    after_pool = dims_after_pooling(start, n_pools)
+    assert after_pool == expected
