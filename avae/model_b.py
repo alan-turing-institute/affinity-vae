bacd6251d1d4db2ee600b3096a5c7d1dfb246b86
Marjan <marjanfamili@gmail.com>
2023-04-21 16:40:45 +0100

    added set_device to a seperate file called device_util.py and removed it from model_b, this allows the device to be loaded for both models

--- avae/model_b.py
+++ avae/model_b.py
@@ -348,12 +348,3 @@ class AffinityVAE(nn.Module):
             return eps.mul(std).add_(mu)
         else:
             return mu
-
-
-def set_device(gpu):
-    device = torch.device(
-        "cuda:0" if gpu and torch.cuda.is_available() else "cpu"
-    )
-    if gpu and device == "cpu":
-        print("\nWARNING: no GPU available, running on CPU instead.\n")
-    return device
