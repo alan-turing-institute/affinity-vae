240f9b7c22c14c87397c1e58f963253055a94fb8
jolaem <jola.mirecka@stfc.ac.uk>
2023-04-21 12:45:59 +0100

    Add some simple data unit tests
    Fixes #56

---
+++ tests/test_data.py
@@ -0,0 +1,85 @@
+import os
+import shutil
+import tempfile
+import unittest
+
+import numpy as np
+from torch.utils.data import DataLoader
+
+from avae.data import load_data
+from tests import testdata
+
+
+class DataTest(unittest.TestCase):
+    def setUp(self) -> None:
+        """Setup data and output directories."""
+        self.test_data = os.path.dirname(testdata.__file__)
+        self.test_dir = tempfile.mkdtemp(prefix="avae_")
+        print(self.test_data, self.test_dir)
+
+        # Change to test directory
+        self._orig_dir = os.getcwd()
+        os.chdir(self.test_dir)
+
+    def tearDown(self):
+        os.chdir(self._orig_dir)
+        if os.path.exists(self.test_dir):
+            shutil.rmtree(self.test_dir)
+
+    def test_load_eval_data(self):
+        """Test loading evaluation data."""
+
+        shutil.copytree(
+            os.path.join(self.test_data, "test"),
+            os.path.join(self.test_dir, "eval"),
+        )
+
+        out = load_data(
+            "./eval", lim=None, batch_s=32, collect_meta=False, eval=True
+        )
+
+        # test load_data
+        assert len(out) == 1
+        eval_data = out
+        assert isinstance(eval_data, DataLoader)
+
+        # test ProteinDataset
+        eval_batch = list(eval_data)[0]
+        xs, ys, aff = eval_batch
+        assert len(xs) == len(ys) == len(aff)
+        assert aff[0] == 0  # this is redundant for eval, should fix in future
+
+    def test_load_train_data(self):
+        """Test loading training data."""
+        shutil.copytree(self.test_data, os.path.join(self.test_dir, "train"))
+
+        out = load_data(
+            "./train",
+            lim=None,
+            splt=30,
+            batch_s=16,
+            no_val_drop=True,
+            collect_meta=False,
+            eval=False,
+            affinity="./train/affinity_fsc_10.csv",
+        )
+
+        # test load_data
+        assert len(out) == 4
+        train_data, val_data, test_data, lookup = out
+        assert len(train_data) >= len(val_data)
+        assert isinstance(train_data, DataLoader)
+
+        # test ProtenDataset
+        train_batch = list(train_data)[0]
+        xs, ys, aff = train_batch
+        assert len(xs) == len(ys) == len(aff)
+        assert aff[0] != 0
+
+        # test affinity matrix
+        assert isinstance(lookup, np.ndarray)
+        assert len(lookup.shape) == 2
+        assert lookup.shape[0] == lookup.shape[1]
+        assert lookup[0][0] == 1
+
+    # write more tests for ProteinDataset
