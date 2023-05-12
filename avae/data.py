b0c499a49251d2e4ce21c33446053be9e189851a
marjanfamili <marjanfamili@gmail.com>
2023-05-12 15:47:20 +0100

    resolving issues #110 #109 #111 Adding f1_score resolving bug in model_a to accept pose=0 choosing classes was wrong , line 178 of data.py should not be in if statement

--- avae/data.py
+++ avae/data.py
@@ -186,8 +186,7 @@ class ProteinDataset(Dataset):
             self.amatrix = self.amatrix.iloc[index, index]
 
         self.paths = [
-            p for p in self.paths for c in self.final_classes if c in p
-        ]
+            p for p in self.paths for c in self.final_classes if c in p]
 
         self.paths = self.paths[:lim]
 
