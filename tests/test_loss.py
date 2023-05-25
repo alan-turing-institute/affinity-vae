bb38003a6e0c2a761d10f573dcb57cde5afeddf8
crangelsmith <crangelsmith@turing.ac.uk>
2023-05-25 16:02:47 +0100

    precommit

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -1,22 +1,28 @@
-from avae.loss import AVAELoss, AffinityLoss
-import torch
 import unittest
-import pandas as pd
+
 import numpy as np
+import pandas as pd
+import torch
+
+from avae.loss import AffinityLoss, AVAELoss
+
 
 class LossTest(unittest.TestCase):
     def setUp(self) -> None:
         """Test instantiation of the loss."""
 
-        self.affinity = pd.read_csv("./testdata/affinity_fsc_10.csv").to_numpy(dtype=np.float32)
-        self.loss = AVAELoss(torch.device("cpu"), [1], [1],lookup_aff=self.affinity,recon_fn="MSE")
-
-
+        self.affinity = pd.read_csv("./testdata/affinity_fsc_10.csv").to_numpy(
+            dtype=np.float32
+        )
+        self.loss = AVAELoss(
+            torch.device("cpu"),
+            [1],
+            [1],
+            lookup_aff=self.affinity,
+            recon_fn="MSE",
+        )
 
     def test_loss_instatiation(self):
         """Test instantiation of the loss."""
 
         assert isinstance(self.loss, AVAELoss)
-
-
-
