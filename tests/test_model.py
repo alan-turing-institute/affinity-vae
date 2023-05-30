2219b18cd04aa8c066e9e6228921005dc8665c34
crangelsmith <crangelsmith@turing.ac.uk>
2023-05-30 11:42:22 +0100

    adding model evaluation test

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,8 +1,9 @@
-from torch import nn, randn, Size
+import unittest
+
+from torch import Size, nn, randn
 
 from avae.model_a import AffinityVAE as avae_a
 from avae.model_b import AffinityVAE as avae_b
-import unittest
 
 
 class ModelTest(unittest.TestCase):
@@ -29,30 +30,36 @@ class ModelTest(unittest.TestCase):
             pose_dims=3,
         )
 
-
     def test_model_instance_a(self):
         """Test instantiation of the model a."""
 
         assert isinstance(self.vae_a, avae_a)
-
 
     def test_model_instance_b(self):
         """Test instantiation of the model a."""
 
         assert isinstance(self.vae_b, avae_b)
 
-
     def test_model_a_3D(self):
         """Test that model is instantiated with 3D convolutions."""
         assert isinstance(self.vae_a.encoder.encoder[0], nn.Conv3d)
         assert isinstance(self.vae_a.decoder.decoder[-1], nn.ConvTranspose3d)
 
-
     def test_model_a_2D(self):
         """Test that model is instantiated with 2D convolutions."""
 
         assert isinstance(self.vae_a_2d.encoder.encoder[0], nn.Conv2d)
-        assert isinstance(self.vae_a_2d.decoder.decoder[-1], nn.ConvTranspose2d)
+        assert isinstance(
+            self.vae_a_2d.decoder.decoder[-1], nn.ConvTranspose2d
+        )
 
-    assert isinstance(vae.encoder.encoder[0], nn.Conv2d)
-    assert isinstance(vae.decoder.decoder[-1], nn.ConvTranspose2d)
+    def test_model_a_eval(self):
+
+        x = randn(14, 1, 64, 64, 64)
+        y = self.vae_a(x)
+
+        self.assertEqual(x.shape, y[0].shape)
+        self.assertEqual(randn(14, 16).shape, y[1].shape)
+        self.assertEqual(randn(14, 16).shape, y[2].shape)
+        self.assertEqual(randn(14, 16).shape, y[3].shape)
+        self.assertEqual(randn(14, 3).shape, y[4].shape)
