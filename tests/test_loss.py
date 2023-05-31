6a05ba53e99773c6a7e504417a557300b0eb59a6
crangelsmith <crangelsmith@turing.ac.uk>
2023-05-31 10:39:02 +0100

    adding test for loss

--- tests/test_loss.py
+++ tests/test_loss.py
@@ -6,7 +6,11 @@ import pandas as pd
 import torch
 
 from avae.loss import AVAELoss
+from avae.model_a import AffinityVAE as avae_a
+from avae.utils import set_device
 from tests import testdata
+
+torch.manual_seed(0)
 
 
 class LossTest(unittest.TestCase):
@@ -27,6 +31,14 @@ class LossTest(unittest.TestCase):
             lookup_aff=self.affinity,
             recon_fn="MSE",
         )
+        self.vae_a = avae_a(
+            capacity=8,
+            depth=4,
+            input_size=(64, 64, 64),
+            latent_dims=16,
+            pose_dims=3,
+        )
+        self.device = set_device(True)
 
     def tearDown(self):
         os.chdir(self._orig_dir)
@@ -35,3 +47,22 @@ class LossTest(unittest.TestCase):
         """Test instantiation of the loss."""
 
         assert isinstance(self.loss, AVAELoss)
+
+    def test_loss(self):
+
+        x = torch.randn(14, 1, 64, 64, 64)
+
+        x_hat, lat_mu, lat_logvar, lat, lat_pose = self.vae_a(x)
+        total_loss, recon_loss, kldivergence, affin_loss = self.loss(
+            x,
+            x_hat,
+            lat_mu,
+            lat_logvar,
+            0,
+            batch_aff=torch.ones(14, dtype=torch.int),
+        )
+
+        self.assertGreaterEqual(total_loss.detach().numpy().item(0), 1.1171)
+        self.assertGreater(recon_loss.detach().numpy().item(0), 1)
+        self.assertGreater(recon_loss, kldivergence)
+        self.assertGreater(total_loss, affin_loss)
