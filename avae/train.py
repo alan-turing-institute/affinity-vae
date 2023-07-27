992eb68121d2a17566eb16209c80744e15203e65
marjanfamili <marjanfamili@gmail.com>
2023-07-27 16:46:06 +0100

    Adding SGD and a key for choosing the optimiser

--- avae/train.py
+++ avae/train.py
@@ -48,6 +48,7 @@ def train(
     recon_fn,
     use_gpu,
     model,
+    opt_method,
     gaussian_blur,
     normalise,
     shift_min,
@@ -113,6 +114,8 @@ def train(
         If True, the model will be trained on GPU.
     model: str
         Type of model to train. Can be a or b.
+    opt_method: str
+        The method of optimisation. It can be adam or sgd
     gaussian_blur: bool
         if True, Gaussian bluring is applied to the input before being passed to the model.
         This is added as a way to remove noise from the input data.
@@ -167,9 +170,19 @@ def train(
 
     vae.to(device)
 
-    optimizer = torch.optim.Adam(
-        params=vae.parameters(), lr=learning  # , weight_decay=1e-5
-    )
+    if opt_method == "adam":
+        optimizer = torch.optim.Adam(
+            params=vae.parameters(), lr=learning  # , weight_decay=1e-5
+        )
+    elif opt_method == "sgd":
+        optimizer = torch.optim.SGD(
+            params=vae.parameters(), lr=learning  # , weight_decay=1e-5
+        )
+    else:
+        raise ValueError(
+            "Invalid optimisation method", opt_method, "must be adam or sgd"
+        )
+
     t_history = []
     v_history = []
     e_start = 0
