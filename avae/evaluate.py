9ecfb625638e9d18cc19890f887ec7dd964deed4
crangelsmith <crangelsmith@turing.ac.uk>
2023-08-10 16:53:37 +0100

    fixing logging in evaluate precommit

--- avae/evaluate.py
+++ avae/evaluate.py
@@ -96,7 +96,7 @@ def evaluate(
     fname = state.split(".")[0].split("_")
     pose_dims = fname[3]
 
-    logging.info("Loading model from: {state}".format(state))
+    logging.info("Loading model from: {}".format(state))
     checkpoint = torch.load(state)
     vae = checkpoint["model_class_object"]
     vae.load_state_dict(checkpoint["model_state_dict"])
