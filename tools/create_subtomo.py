d37d7a9f214c2905d979bf6973d8c9a6cfd51407
marjanfamili <marjanfamili@gmail.com>
2023-08-18 14:37:11 +0100

    Added augmentation per voxel

--- tools/create_subtomo.py
+++ tools/create_subtomo.py
@@ -1,16 +1,32 @@
-import mrcfile
+import os
 import warnings
-import pandas as pd
-from scipy.ndimage import rotate
-import torch
+
+import mrcfile
 import numpy as np
-import os 
+import pandas as pd
+import torch
+from scipy.ndimage import rotate
 
 
-def create_subtomo(input_path, output_path, datatype,  annot_path , vox_size= [32,32,32],bandpass=False, low_freq = 0  , high_freq = 15,gaussian_blur= False,
-                  add_noise =False , noise_int = 0, padding=False, augment, aug_th_min
-    ):
-
+def create_subtomo(
+    input_path,
+    output_path,
+    datatype,
+    annot_path,
+    vox_size=[32, 32, 32],
+    bandpass=False,
+    low_freq=0,
+    high_freq=15,
+    gaussian_blur=False,
+    add_noise=False,
+    noise_int=0,
+    padding=False,
+    padded_size=[32, 32, 32],
+    augment=False,
+    aug_num=5,
+    aug_th_min=-45,
+    aug_th_max=45,
+):
     """Function to train an AffinityVAE model. The inputs are training configuration parameters. In this function the
     data is loaded, selected and split into training, validation and test sets, the model is initialised and trained
     over epochs, the results are evaluated visualised and saved and the epoch level with a frequency configured with
@@ -21,64 +37,89 @@ def create_subtomo(input_path, output_path, datatype,  annot_path , vox_size= [3
     input_path: str
         Path to the folder containing the full tomogram/image.
     output_path: str
-        Path to the folder for output subtomograms 
+        Path to the folder for output subtomograms
     datatype : str
         data file formats : mrc, npy
     annot_path : str
-        path to the folder containing the name of the particles and their x,y,z coordinates 
-        file names should be identical to that of the full tomograms with the extension of txt 
+        path to the folder containing the name of the particles and their x,y,z coordinates
+        file names should be identical to that of the full tomograms with the extension of txt
         for mat of the txt files is ,  'class','z','y','x','cx','cy','cz
-    vox_size: list 
+    vox_size: list
         size of each subtomogram voxel given as a list where vox_size: [x,y,x]
-    bandpass: bool 
-        Apply band pass filter to the full tomogram before extracting the subtomograms 
+    bandpass: bool
+        Apply band pass filter to the full tomogram before extracting the subtomograms
     low_freq: int
-        Lower frequency threshold for the band pass filter 
+        Lower frequency threshold for the band pass filter
     high_freq: int
-        higher frequency threshold for the band pass filter 
+        higher frequency threshold for the band pass filter
 
     """
 
-    # the name of all full tomograms 
-    file_list = [
-        f for f in os.listdir(input_path) if "." + datatype in f
-    ]
+    # the name of all full tomograms
+    file_list = [f for f in os.listdir(input_path) if "." + datatype in f]
 
-    # the name of all annotations for tomograms 
-    annot_list = [f for f in os.listdir(input_path) if "." + "txt" in f
-    ]
+    # the name of all annotations for tomograms
+    annot_list = [f for f in os.listdir(input_path) if "." + "txt" in f]
 
     if len(annot_list) != len(file_list):
-        raise RuntimeError("number of full tomograms do not match the number of annotation files"
+        raise RuntimeError(
+            "number of full tomograms do not match the number of annotation files"
         )
 
-
-    for t, tomo  in enumerate(file_list):
+    for t, tomo in enumerate(file_list):
         data = read_mrc(os.path.join(input_path, tomo))
         if bandpass:
-            bandpass_filter_image = bandpass_filter(data.shape, low_freq,high_freq)
+            bandpass_filter_image = bandpass_filter(
+                data.shape, low_freq, high_freq
+            )
             data = apply_bandpass_filter(data, bandpass_filter_image)
         if add_noise:
-            data = add_noise(data, noise_int)
+            data = add_g_noise(data, noise_int)
         if gaussian_blur:
             print("gaussian blur is not implemented yet")
-            data =data
-        
+            data = data
 
-     
+        particle_df = particles_GT(
+            os.path.join(annot_path, annot_list[t]), output_path
+        )
+
+        particle_df = delete_detection_in_edges(
+            particle_df, data.shape, vox_size
+        )
+
+        for index, protein in particle_df.iterrows():
+            name = protein["class"]
+            s = [int(n / 2) - more for n in vox_size]
+            mol = data[
+                protein["x"] - s[0] : protein["x"] + s[0],
+                protein["y"] - s[1] : protein["y"] + s[1],
+                protein["z"] - s[2] : protein["z"] + s[2],
+            ]
+            if augment:
+                mol = augmentation(mol, aug_num, aug_th_min, aug_th_max)
 
 
+def augmentation(mol, aug_num, aug_th_min, aug_th_max):
+    deg_per_rot = 15
+    for i in range(aug_num):
+        angle = np.random.randint(
+            aug_th_min, aug_th_max, size=(len(mol.shape),)
+        )
+
+        for ax in range(angle.size):
+            theta = angle[ax] * deg_per_rot
+            axes = (ax, (ax + 1) % angle.size)
+            mol = rotate(mol, theta, axes=axes, order=0, reshape=False)
 
 
-def bandpass_filter(image_size,bp_low,bp_high):
+def bandpass_filter(image_size, bp_low, bp_high):
 
     bandpass_filter = np.zeros((image_size), dtype=np.float32)
     if len(image_size) == 2:
         for u in range(image_size[0]):
             for v in range(image_size[1]):
                 D = np.sqrt(
-                    (u - image_size[0] / 2) ** 2
-                    + (v - image_size[1] / 2) ** 2
+                    (u - image_size[0] / 2) ** 2 + (v - image_size[1] / 2) ** 2
                 )
                 if D <= bp_low:
                     bandpass_filter[u, v] = 1
@@ -101,69 +142,73 @@ def bandpass_filter(image_size,bp_low,bp_high):
     return bandpass_filter
 
 
-def apply_bandpass_filter(image,bandpass_filter):
+def apply_bandpass_filter(image, bandpass_filter):
     F = np.fft.fftn(image)
-    Fshift = np.fft.fftshift(F) 
+    Fshift = np.fft.fftshift(F)
     Gshift = Fshift * bandpass_filter
     G = np.fft.ifftshift(Gshift)
     filtered_image = abs(np.fft.ifftn(G))
-    return filtered_image.astype('float32')
+    return filtered_image.astype("float32")
 
 
-def add_noise(input_a,method== 'gaussian',scale):
+def add_g_noise(input_a, scale):
+    # Gaussian distribution parameters
+    mean = 0
+    var = 0.1
+    sigma = var**0.5
 
-    if method == 'gaussian':
-        # Gaussian distribution parameters
-        mean = 0
-        var = 0.1
-        sigma = var ** 0.5
+    gaussian = np.random.normal(
+        mean, sigma, (input_a.shape[0], input_a.shape[1], input_a.shape[1])
+    )
 
-        gaussian = np.random.normal(mean, sigma, (input_a.shape[0],input_a.shape[1], input_a.shape[1])) 
-
-        output_a = input_a + gaussian * scale
+    output_a = input_a + gaussian * scale
     return output_a.astype(np.float32)
 
+
 def read_mrc(path):
-    '''
-    Takes a path and read a mrc file convert the data to np array 
-    '''
-    warnings.simplefilter('ignore') # to mute some warnings produced when opening the tomos
-    with mrcfile.open(path, mode='r+', permissive=True) as mrc:
+    """
+    Takes a path and read a mrc file convert the data to np array
+    """
+    warnings.simplefilter(
+        "ignore"
+    )  # to mute some warnings produced when opening the tomos
+    with mrcfile.open(path, mode="r+", permissive=True) as mrc:
         mrc.update_header_from_data()
 
-        mrc.header.map = mrcfile.constants.MAP_ID 
-        mrc = mrc.data       
+        mrc.header.map = mrcfile.constants.MAP_ID
+        mrc = mrc.data
 
     with mrcfile.open(path) as mrc:
         data = np.array(mrc.data)
     return data
 
 
+def particles_GT(annot, output_path):
 
-def particles_GT(annot,output_path):
+    labels = np.loadtxt(annot, dtype="str")
 
-    labels = np.loadtxt(annot,dtype='str')
+    labels = np.reshape(labels, (-1, 7))
+    df = pd.DataFrame(labels)
+    df.columns = ["class", "z", "y", "x", "cx", "cy", "cz"]
 
-    labels = np.reshape(labels,(-1,7))
-    df = pd.DataFrame (labels)
-    df.columns =['class','z','y','x','cx','cy','cz']
+    df = df.astype({"x": "int"})
+    df = df.astype({"y": "int"})
+    df = df.astype({"z": "int"})
+    proteins = df["class"].unique()
 
-    df = df.astype({'x':'int'})
-    df = df.astype({'y':'int'})
-    df = df.astype({'z':'int'})
-    proteins = df['class'].unique()
-    
-    df.drop(df[df['class'] == 'vesicle'].index, inplace = True)
+    df.drop(df[df["class"] == "vesicle"].index, inplace=True)
 
-    with open(f"{folder_to_copy_to}/classes.csv", 'w') as f:
-        f.write(','.join(proteins))
+    with open(f"{output_path}/classes.csv", "w") as f:
+        f.write(",".join(proteins))
     return df
 
-def delete_detection_in_edges(df, X_dim,Y_dim,Z_dim): 
-    df.drop(df[df['x'] > X_dim-slice_size].index, inplace = True)
-    df.drop(df[df['y'] > Y_dim-slice_size].index, inplace = True)
-    df.drop(df[df['z'] > Z_dim-slice_size].index, inplace = True)
-    df.drop(df[df['x'] <   slice_size].index, inplace = True)
-    df.drop(df[df['y'] <   slice_size].index, inplace = True)
-    df.drop(df[df['z'] <   slice_size].index, inplace = True)
+
+def delete_detection_in_edges(df, data_shape, vox_size):
+    # drop the particles that are appearing in the edges
+    df.drop(df[df["x"] > data_shape[0] - vox_size[0]].index, inplace=True)
+    df.drop(df[df["y"] > data_shape[1] - vox_size[1]].index, inplace=True)
+    df.drop(df[df["z"] > data_shape[2] - vox_size[2]].index, inplace=True)
+    df.drop(df[df["x"] < vox_size[0]].index, inplace=True)
+    df.drop(df[df["y"] < vox_size[1]].index, inplace=True)
+    df.drop(df[df["z"] < vox_size[2]].index, inplace=True)
     return df
