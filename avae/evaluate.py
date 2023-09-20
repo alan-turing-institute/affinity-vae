f0ac4938309502893c6a6515f153ad26bb6bd24b
marjanfamili <marjanfamili@gmail.com>
2023-09-20 11:13:15 +0100

    the umap embeddings should take in the latent (z) but not mu

--- avae/evaluate.py
+++ avae/evaluate.py
@@ -132,7 +132,7 @@ def evaluate(
         x, x_hat, lat_mu, lat_logvar, lat, lat_pose, _ = pass_batch(
             device, vae, batch, b, len(tests)
         )
-        x_test.extend(lat_mu.cpu().detach().numpy())
+        x_test.extend(lat.cpu().detach().numpy())
         c_test.extend(lat_logvar.cpu().detach().numpy())
 
         # if labels are present save them otherwise save test
