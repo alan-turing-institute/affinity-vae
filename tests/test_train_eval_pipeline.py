12c1d281e754400e7aa9610d5834510f800c600c
jolaem <jola.mirecka@stfc.ac.uk>
2023-10-20 10:05:22 +0100

    Add test for model B 2D case and fix model A var

--- tests/test_train_eval_pipeline.py
+++ tests/test_train_eval_pipeline.py
@@ -140,7 +140,35 @@ class TrainEvalTest(unittest.TestCase):
         self.data["model"] = "a"
         self.data["datatype"] = "npy"
         self.data["datapath"] = self.testdata_npy
-        config.VIS_POSE_VIS = ["2", "5"]
+        config.VIS_POSE_CLASS = ["2", "5"]
+
+        self.data["affinity"] = os.path.join(
+            self.testdata_npy, "affinity_an.csv"
+        )
+        self.data["classes"] = os.path.join(self.testdata_npy, "classes.csv")
+        (
+            n_dir_train,
+            n_plots_train,
+            n_latent_train,
+            n_states_train,
+            n_plots_eval,
+            n_latent_eval,
+            n_states_eval,
+        ) = helper_train_eval(self.data)
+
+        self.assertEqual(n_dir_train, 4)
+        self.assertEqual(n_plots_train, 28)
+        self.assertEqual(n_latent_train, 2)
+        self.assertEqual(n_states_train, 2)
+        self.assertEqual(n_plots_eval, 45)
+        self.assertEqual(n_latent_eval, 4)
+        self.assertEqual(n_states_eval, 3)
+
+    def test_model_b_npy(self):
+        self.data["model"] = "b"
+        self.data["datatype"] = "npy"
+        self.data["datapath"] = self.testdata_npy
+        config.VIS_POSE_CLASS = ["2", "5"]
 
         self.data["affinity"] = os.path.join(
             self.testdata_npy, "affinity_an.csv"
