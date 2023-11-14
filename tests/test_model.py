0a8ee0ccb5cfdae3458d1b8b38ec0c0ec7d773bc
crangelsmith <crangelsmith@turing.ac.uk>
2023-11-14 15:21:16 +0000

    updating model tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,12 +2,24 @@ import unittest
 
 from torch import nn, randn
 
-from avae.model import AffinityVAE as avae
+from avae.decoders.decoders import Decoder
+from avae.encoders.encoders import Encoder
+from avae.models import AffinityVAE as avae
 
 
 class ModelTest(unittest.TestCase):
     def setUp(self) -> None:
-        self.vae = avae(
+
+        self.encoder_3d = Encoder(
+            capacity=8,
+            depth=4,
+            input_size=(64, 64, 64),
+            latent_dims=16,
+            pose_dims=3,
+            bnorm=True,
+        )
+
+        self.decoder_3d = Decoder(
             capacity=8,
             depth=4,
             input_size=(64, 64, 64),
@@ -15,13 +27,24 @@ class ModelTest(unittest.TestCase):
             pose_dims=3,
         )
 
-        self.vae_2d = avae(
+        self.vae = avae(self.encoder_3d, self.decoder_3d)
+
+        self.encoder_2d = Encoder(
             capacity=8,
             depth=4,
             input_size=(64, 64),
             latent_dims=16,
             pose_dims=3,
         )
+        self.decoder_2d = Decoder(
+            capacity=8,
+            depth=4,
+            input_size=(64, 64),
+            latent_dims=16,
+            pose_dims=3,
+        )
+
+        self.vae_2d = avae(self.encoder_2d, self.decoder_2d)
 
     def test_model_instance(self):
         """Test instantiation of the model."""
