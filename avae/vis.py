751c1fbad97ee18a66a24ab3cf405b87775e4d48
marjanfamili <marjanfamili@gmail.com>
2024-02-02 15:57:29 +0000

    added more flexibile options to plotting functions

--- avae/vis.py
+++ avae/vis.py
@@ -311,6 +311,7 @@ def latent_embed_plot_umap(
     writer: typing.Any = None,
     rs: int = 42,
     marker_size: int = 24,
+    l_w: int = 2,
     display: bool = False,
 ) -> None:
     """Plot static UMAP embedding.
@@ -331,6 +332,8 @@ def latent_embed_plot_umap(
         Tensorboard summary writer
     marker_size: int
         Marker size for UMAP plot
+    l_w: int
+        Line width for the historgram plot when the pose is one dimensional. 
     display: bool
         When this variable is set to true, the save_imshow_png function only dispalys the plot
         and does not save a png image. This is to allow the use of this function in jupyter notebook
@@ -421,6 +424,7 @@ def latent_embed_plot_umap(
                 stacked=True,
                 fill=False,
                 label=mol[:4],
+                linewidth=l_w,
             )
         plt.legend(
             prop={"size": 10},
@@ -1458,6 +1462,7 @@ def pose_class_disentanglement_plot(
     device: torch.device,
     mode: str = "trn",
     number_of_samples: int = 7,
+    specific_enc: npt.NDArray = None,
     display: bool = False,
 ):
 
@@ -1514,6 +1519,9 @@ def pose_class_disentanglement_plot(
         class_x = np.take(x, np.where(np.array(y) == i)[0], axis=0)
         class_x_indx = np.random.choice(class_x.shape[0])
         enc = class_x[class_x_indx, :]
+
+        if specific_enc is not None:
+            enc = specific_enc
 
         class_pos = np.take(poses_space, np.where(np.array(y) == i)[0], axis=0)
         class_pos_mean = np.mean(class_pos, axis=0)
